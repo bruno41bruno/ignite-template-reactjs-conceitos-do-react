5e420efeeda6324f76a0c489674598dd77e0736d
Bruno Gabriel <celilvr13@gmail.com>
2021-12-19 01:37:41 -0300

    desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,27 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== ''){
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+      setTasks([...tasks,newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const objTask = tasks.find((x: Task) => x.id === id)
+    objTask.isComplete = !objTask.isComplete;
+    setTasks([...tasks]);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    tasks.splice(tasks.findIndex((x: Task) => x.id === id),[1]);
+    setTasks([...tasks]);
   }
 
   return (
